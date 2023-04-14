fef800c00ca4f07a8c2fc20a4637c7554ee0190b
fadhil <88018547+vadhil@users.noreply.github.com>
2023-04-15 00:37:01 +0800

    sisa delete dan update

--- app.js
+++ app.js
@@ -1,78 +1,19 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const {MongoClient} = require('mongodb');
-const mongoose = require('mongoose');
+const {readContacts, Contact} = require('./utils/function');
 
-// const {findContacts} = require('./utils/function');
+
+const bodyParser = require('body-parser');
+
 
 
 const app = express();
-const url = "mongodb+srv://vadhil:Alhafidz135@cluster0.pclmzog.mongodb.net/mongoDB";
-const client = new MongoClient(url);
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
-mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then((con) => {
-    console.log("Connected to database");
-})
-
-const contactSchema =  new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    phone: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    email: {
-        type: String,
-        unique: true
-    },
-})
-
-const Contact =  mongoose.model('Contact', contactSchema);
-
-const person1 = new Contact({
-    name: "fakhri",
-    phone: "089539545744",
-    email: "fakhri135@gmail.com"
-})
-const person2 = new Contact({
-    name: "fathul",
-    phone: "089535455744",
-    email: "fahmimajid01@gmail.com"
-})
-
-// const contacts = Contact.find()
-// .then(users => console.log(users.length))
-// .catch(err => console.log(`OH NOOO, ${err.message}`));
-
-
-Contact.find()
-    .then(data => {
-        // const contacts = data;
-        console.log(data);
-        // return contacts;
-    })
-    .catch(error => {
-        console.log(error);
-});
-
-const contacts = Contact.find();
-
-// console.log(typeof(contacts));
-// console.log(contacts);
-
-// person2.save().then((doc) => {console.log(doc)}).catch((err) => {console.log(err)});
 
   //layout
 
@@ -98,7 +39,7 @@ app.get('/playground', (req, res) => {
 // }) contoh yang salah
 app.get('/contact', async (req, res) => {
     try {
-        const contacts = await Contact.find();
+        const contacts = await readContacts();
         res.render('contact', {
             layout: 'layout/main',
             title: "contact ",
@@ -109,18 +50,51 @@ app.get('/contact', async (req, res) => {
         res.status(500).send("Internal Server Error");
     }
 });
+app.post('/contact', (req, res) => {
+    const contact = new Contact({
+      name: req.body.name,
+      phone: req.body.phone,
+      email: req.body.email,
+    });
+  
+    contact.save()
+      .then(() => {
+        console.log('Contact added successfully!');
+        res.redirect('/contact');
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send('Internal server error');
+      });
+  });
 app.get('/add-contact', (req, res) => {
     res.render('add-contact', {
         layout: 'layout/main',
         title: "add contact form"
     })
 })
-app.get('/detail', (req, res) => {
+// app.get('/:name/deleted', async (req, res) => {
+//     const contact= await readContacts({name : req.params.name});
+//     Contact.deleteOne({ name: 'John Doe' })
+//   .then(() => {
+//     console.log('Contact deleted successfully!');
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+//     res.render('add-contact', {
+//         layout: 'layout/main',
+//         title: "add contact form"
+//     })
+// })
+app.get('/contact/:name',  async(req, res) => {
+    const contact= await readContacts({name : req.params.name});
+    // console.log(contact[0].name);
     res.render('detail', {
         layout: 'layout/main',
-        title: "detail contact "
-    })
-})
+        title: "detail contact",
+        contact,
+    })})
 app.get('/about', (req, res) => {
     res.render('about', {
         layout: 'layout/main',
--- utils/function.js
+++ utils/function.js
@@ -28,23 +28,45 @@ const contactSchema =  new mongoose.Schema({
         unique: true
     },
 })
-
 const Contact =  mongoose.model('Contact', contactSchema);
 
 Contact.find()
-    .then(data => {
-        // const contacts = data;
-        console.log(data);
-        // return contacts;
+    .then(()=> {
     })
     .catch(error => {
         console.log(error);
 });
+function readContacts(query) {
+    return Contact.find(query);
+}
+
+const person1 = new Contact({
+    name: "keysha",
+    phone: "0895349545744",
+    email: "keysha@gmail.com"
+})
+
+// Contact.deleteOne()
+//   .then(result => {
+//     console.log(`Deleted ${result.deletedCount} document(s)`);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+// function deleteContacts(query) {
+//     return Contact.deleteOne(query);
+// }
+
+
+// toSave 
+// person1.save().then((doc) => {console.log(doc)}).catch((err) => {console.log(err)});
+
+
+  
+  module.exports ={ readContacts, Contact};
 
 // const readContacts = Contact.find();
 
-// toSave 
-// person2.save().then((doc) => {console.log(doc)}).catch((err) => {console.log(err)});
+// deleteContacts
 
-
-module.exports = Contact.find;
+// module.exports = Contact.find;
